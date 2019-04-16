defeab23b0b355beaf5c97654f8e26f2ffa460ee
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-16 08:22:40 -0700

    ContentChild

--- src/app/lib/au-fa-input/au-fa-input.component.ts
+++ src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
 
 @Component({
   selector: 'au-fa-input',
   templateUrl: './au-fa-input.component.html',
   styleUrls: ['./au-fa-input.component.css']
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements OnInit, AfterContentInit {
 
   @Input() icon: string;
-  @Input() placeholder = '';
+  @ContentChild(HTMLInputElement) input: HTMLInputElement;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    console.log('input', this.input);
   }
 
   get classes() {
