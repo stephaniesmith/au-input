e860b05e8020f0d29cadabb89bd55edf40f35373
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-04-23 08:12:26 -0700

    working on the tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,47 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { AuFaInputComponent } from './lib/au-fa-input/au-fa-input.component';
+import { InputRefDirective } from './lib/common/input-ref.directive';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+    let el: DebugElement;
+    let emailField: DebugElement;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                AuFaInputComponent,
+                InputRefDirective
             ],
         }).compileComponents();
     }));
 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.debugElement.componentInstance;
+        el = fixture.debugElement;
+        emailField = el.query(By.css('#email-field'));
+    })
+
     it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
+        expect(component).toBeTruthy();
     }));
 
+    it('should create a font awesome email input', async(() => {
+        expect(emailField).toBeTruthy();
+    }));
 
+    it('should include the correct email icon inside the email input', async(() => {
+        expect(emailField.query(By.css('i.icon.fa.fa-envelope'))).toBeTruthy();
+    }));
 
-
-
-
+    it('should have projected the correct test input inside the email field', async(() => {
+        expect(emailField.query(By.css('input.test-class'))).toBeTruthy();
+    }));
 });
